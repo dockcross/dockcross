566251b39639c01cdcf1b3e2efb30fd3720abbc2
Andreas Schneider <asn@cryptomilk.org>
2019-10-30 10:38:21 +0100

    linux-mips: Define include and library path

--- linux-mips/Toolchain.cmake
+++ linux-mips/Toolchain.cmake
@@ -9,6 +9,15 @@ set(CMAKE_C_COMPILER $ENV{CC})
 set(CMAKE_CXX_COMPILER $ENV{CXX})
 set(CMAKE_Fortran_COMPILER $ENV{FC})
 
+# Search path for MIPS libraries
+#   dpkg --add-architecture "mips"
+#   apt-get install libcmocka-dev:mips
+set(CMAKE_INCLUDE_PATH "/usr/lib/mips-include-gnu")
+set(CMAKE_LIBRARY_PATH "/usr/lib/mips-linux-gnu")
+
+set(CMAKE_C_FLAGS "-I${cross_root}/include/ -I${CMAKE_INCLUDE_PATH}")
+set(CMAKE_CXX_FLAGS "-I${cross_root}/include/ -I${CMAKE_INCLUDE_PATH}")
+
 set(CMAKE_FIND_ROOT_PATH ${cross_root} ${cross_root}/${cross_triple})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
